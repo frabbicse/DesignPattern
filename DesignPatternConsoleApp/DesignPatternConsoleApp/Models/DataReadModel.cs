b483e18f81bec9b2e42c5324fabb24b290855881
Fazle Rabbi <fazle.rabbi-v@adityabirlacapital.com>
2024-10-29 11:26:32 +0600

    dd

---
+++ DesignPatternConsoleApp/DesignPatternConsoleApp/Models/DataReadModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternConsoleApp.Models
+{
+    public class DataReadModel
+    {
+        public string? AsAccountC { get; set; }
+        public string? CourseOrganizationInstitutionC { get; set; }
+        public string? EmployerC { get; set; }
+        public string? OrganizationC { get; set; }
+        public string? RealStateAgencyC { get; set; }
+        public string? RegisteredTraingOrgTROC { get; set; }
+        public string? RentalAgencyC { get; set; }
+    }
+}
